86c82ab2a23862945085b59a23551743380be1a1
= <=>
2025-10-23 11:03:44 +0530

    Adding the react project along with test files

---
+++ simple-reactVite-testapp/App.test.jsx
@@ -0,0 +1,21 @@
+
+import React from "react";
+import { render,screen } from "@testing-library/react";
+import App from "./src/App";
+import { describe, it, expect } from "vitest";
+import "@testing-library/jest-dom/vitest"
+
+
+describe("Checking if the word - welcome exists?", () => {
+
+    it("checking if a word exists", () => {
+        render(<App />);
+        expect(screen.getByText(/Welcome/i)).toBeInTheDocument();
+    });
+
+    it("checking if a word exists", () => {
+        render(<App />);
+        expect(screen.getByText(/Test/i)).toBeInTheDocument();
+    });
+
+});
