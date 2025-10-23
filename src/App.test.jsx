a3f3a7c2effe9c91c190cf3969535100cbe0b35c
= <=>
2025-10-23 16:14:08 +0530

    adding react app files

---
+++ src/App.test.jsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom"
+import App from "./App";
+import { describe, it, expect } from "vitest";
+
+describe("Looking for a word", () => {
+    it("Looking for welcome text", () => {
+        render(<App />);
+        expect(screen.getByText(/Welcome/i)).toBeInTheDocument;
+    })
+});
+
+
